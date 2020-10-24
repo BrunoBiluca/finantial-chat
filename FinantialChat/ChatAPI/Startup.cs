ee6b675026aa0f7fbf15594b78384177721ac89e
bruno <bruno.costa@cinnecta.com>
2020-10-23 23:00:24 -0300

    Initital project

---
+++ FinantialChat/ChatAPI/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace ChatAPI {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+
+            services.AddSwaggerGen(o =>
+                o.SwaggerDoc("v1", new OpenApiInfo { Title = "Chat Api", Version = "v1" }));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if(env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v1/swagger.json", "LeaderBord Api V1"));
+        }
+    }
+}
---
+++ plan.md
@@ -0,0 +1,42 @@
+# Financial Chat
+
+# Features
+
+## Main features
+
+- Chat rooms
+  - Group of people to rang out and talk about stocks
+
+- Stock consultant bot
+  - Allow users to search stock prices
+
+- Authentication
+  - Users will be authenticated by .NET Identity 
+
+## Non Functional Features
+
+- Unit testing coverage all funcionalities
+- Persist user's messages
+- Documentation all features
+
+# Project Architeture
+
+![project_arquiteture.png](project_arquiteture.png)
+
+# Task
+
+- ChatAPI
+  - POST messages
+  - 
+
+- StockConsultantBot
+
+- FrontEnd
+  - Chat user interface
+
+- Authentication
+
+# Questions:
+ - What is build an installer?
+   - Maybe is the web ap ready to deploy
+ 
